1adbe233e95665d91ba7a1a1c2b3bbd1b4c541cb
Mikew24 <33185698+Mikew24@users.noreply.github.com>
2017-10-31 16:28:48 -0400

    update

--- PycharmProjects/untitled/lab06_a1.py
+++ PycharmProjects/untitled/lab06_a1.py
@@ -1,4 +1,8 @@
+#Michael Waldron/100657864
+#Character class
+#copied and modified code from my assignment one which we decided to use classes for
 class Character:
+    #constructor sets magic to zero unless other specified
     def __init__(self, name, hp, xp, attack, defense,magic=0):
         self.name = name
         self.maxHP=hp
@@ -7,12 +11,15 @@ class Character:
         self.attack = attack
         self.defense = defense
         self.magic=magic
+       #prints character name and how much Hp they have
     def __str__(self):
         return self.name + ' has '+str(self.hp)+' HP'
+    #checks if the a Character is dead
     def isDead(self):
         if self.hp<=0:
             return True
         return False
+    """ heals party and makes sure hp does not exceed that characters initial full hp"""
     def heal(self, party):
         if not self.isDead():
             for healed in party:
@@ -21,10 +28,11 @@ class Character:
                     print(self.name, 'healed', healed.name, 'for', self.magic, 'hp')
                     if healed.hp>healed.maxHP:
                         healed.hp = healed.maxHP
-
+    #attack method which takes the attacking character and the victim as parameters
     def attacking(self, otherCharacter):
         if self.isDead():
             print(self.name, 'cannot attack because they are dead!')
+
         else:
             if otherCharacter.isDead()==False:
                 damageDealt = self.attack - otherCharacter.defense
@@ -37,7 +45,7 @@ class Character:
                 if not otherCharacter.isDead():
                     print(self.name, 'does', damageDealt, 'points of damage to', otherCharacter.name)
 
-
+#runs the .isdead() method for all characters in the party and returns True if they are all dead
 def isPartyDead(party):
     y=0
     for i in party:
@@ -46,13 +54,14 @@ def isPartyDead(party):
     if y==3:
         return True
 
-
+#initializing Characters
 krogg = Character ('Krogg',180,20,20,10)
 glinda = Character ('Glinda', 120, 5, 20, 5,10)
 geoffrey = Character ('Geoffrey', 150, 15, 15,10)
 party = [krogg, glinda, geoffrey]
 boss = Character('Boss', 500, 25, 15,10)
 round = 1
+#battle loop from code given
 while (not boss.isDead()) and (not isPartyDead(party)):
     print('Round', round)
 
